6ff83765f231946480ccc766c5f0f9e68c4e9ffa
Manuel Haro <mharo@DN525igf.SUNet>
2017-07-13 16:19:50 -0700

    manuel initial code

---
+++ SetUp/accounts/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render, redirect
+from accounts.forms import (
+    RegistrationForm,
+    EditProfileForm
+)
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+
+
+
+
+
+
+
+def home(request):
+    numbers = [1, 2, 3, 4, 5]
+    name = 'Manuel'
+
+    args = {'myName': name, 'numbers': numbers}
+    return render(request, 'accounts/home.html', args)
+
+
+
+def register(request):
+    if request.method =='POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/account')
+    else:
+        form = RegistrationForm()
+
+        args = {'form': form}
+        return render(request, 'accounts/reg_form.html', args)
+
+
+@login_required
+def view_profile(request):
+    print('this is the profile profile page')
+    args = {'user': request.user}
+    return render(request, 'accounts/profile.html', args)
+
+
+@login_required
+def edit_profile(request):
+        if request.method == 'POST':
+            form = EditProfileForm(request.POST, instance=request.user)
+
+            if form.is_valid():
+                form.save()
+                return redirect('/account/profile')
+
+        else:
+            form = EditProfileForm(instance=request.user)
+            args = {'form': form}
+            return render(request, 'accounts/edit_profile.html', args)
+
+@login_required
+def change_password(request):
+        if request.method == 'POST':
+            form = PasswordChangeForm(data=request.POST, user=request.user)
+
+            if form.is_valid():
+                form.save()
+                update_session_auth_hash(request, form.user)
+                return redirect('/account/profile')
+
+            else:
+                return redirect('/account/change-password')
+
+        else:
+            form = PasswordChangeForm(user=request.user)
+            args = {'form': form}
+            return render(request, 'accounts/change_password.html', args)
+
+
+#This is the graphing stuff, its not complete
+#def graph(request):
+#canvas=FigureCanvas(fig)
+#return response
